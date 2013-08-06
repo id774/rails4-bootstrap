e5126688927b282213664bcd35ff337507f21da0
id774 <idnanashi@gmail.com>
2013-08-06 11:46:14 +0900

    18 Fix FormTagHelper options

--- config/initializers/submit_with_disable.rb
+++ config/initializers/submit_with_disable.rb
@@ -3,7 +3,8 @@ module ActionView
     module FormTagHelper
       alias_method :original_submit_tag, :submit_tag
       def submit_tag(value=nil, options={})
-        original_submit_tag(value, :data => { disable_with: 'Sending...' })
+        options[:data] = { disable_with: 'Sending...' } unless options[:data]
+        original_submit_tag(value, options)
       end
     end
   end
