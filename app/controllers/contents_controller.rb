cc0a6516353fb7982c9b614688d0768f1d5dd0b1
id774 <idnanashi@gmail.com>
2013-08-06 11:26:01 +0900

    17 Move pagination renderer to application controller

--- app/controllers/contents_controller.rb
+++ app/controllers/contents_controller.rb
@@ -58,24 +58,3 @@ class ContentsController < ApplicationController
       params.require(:content).permit(:name, :body)
     end
 end
-
-class BootstrapPaginationRenderer < WillPaginate::ActionView::LinkRenderer
-  private
-  def previous_or_next_page(page, text, classname)
-    link(text, page, :class => classname) unless page == false
-  end
-
-  public
-  def to_html
-    html = pagination.map do |item|
-      tag(:li,
-        ((item.is_a?(Fixnum))?
-          page_number(item) :
-          send(item)))
-    end.map{|x|x.gsub(/><em/, " class=\"active\"><a").gsub(/em/, "a")}.join(@options[:link_separator])
-
-    html = tag(:ul, html)
-
-    @options[:container] ? html_container(html) : html
-  end
-end
